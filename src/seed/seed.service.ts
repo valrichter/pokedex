1f7fcfff106ed6366ce579fcebab91d29900fa4d
valrichter <valenrichter11@gmail.com>
2023-08-10 16:34:25 +0000

    add axios get to pokeapi

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  exeutedSeed() {
-    return `This action returns all seed`;
+  private readonly axios: AxiosInstance = axios;
+
+  async exeutedSeed() {
+    const { data } = await axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+      console.log(no, name);
+    });
+
+    return data.results;
   }
 }
